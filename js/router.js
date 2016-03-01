e372c691fef0c2e50de96a3927f227e2c19c830c
Hellbander <mihails.mihins@inbox.lv>
2016-03-01 16:10:30 +0200

    Saving

--- js/router.js
+++ js/router.js
@@ -1,16 +1,8 @@
-define(['jquery', 'underscore', 'backbone', 'views/index'],
-function ($, _, Backbone, IndexView) {
-  var SiteRouter = Backbone.Router.extend({
-    routes: {
-      '*other': 'defaultAction'
-    }
-  });
-  var initialize = function () {
-    var siteRouter = new SiteRouter();
-    siteRouter.on('route:defaultAction', function(){
-      console.log('default');
-    });
-    Backbone.history.start();
+
+define(['router'],
+function (SiteRouter) {
+  var initialize = function(){
+    SiteRouter.initialize();
   };
   return {
     initialize: initialize
